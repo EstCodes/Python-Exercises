284f01aedd782c3ba8a433797b6b977deaf4be17
Esteban Raigoza <91690458+EstCodes@users.noreply.github.com>
2025-08-11 14:30:57 +0000

    Changes to be committed: modified:   passwordGen.py

--- passwordGen.py
+++ passwordGen.py
@@ -1,4 +1,8 @@
 import random
+import json
+import string
+import os
+from datetime import datetime
 
 class GenPassword:
     def __init__(self, keyword: str = "", keynumbers: int = 0):
@@ -69,6 +73,86 @@ To execute this program you should enter a keyword, this is to remember the asso
         final_password = "".join(password)
         return final_password
     
+    def getStorage(self, label, entry_id, password, filename="passwords.json"):
+        print("""This step is not mandatory. Do you want to save your password on a local file?
+This is actually much easier option than save your passwords on the cloud...""")
+        
+        optionConfirm = input("Type y/n (Yes or No): ")
+
+        state3 = True
+
+        while state3:
+            if optionConfirm == "y" or optionConfirm == "yes":
+                print("Alright so lets continue with the process.")
+                print("-----------------------------------")
+                print("You have a storage file?")
+
+                optionConFile = input("Type y/n (Yes or No): ")
+
+                state4 = True
+
+                while state4:
+                    if optionConFile == "y" or optionConFile == "yes" or activation == True:
+                        print("Okay lets find where is your file...")
+
+                        if os.path.exists(filename):
+                            with open(filename, "r") as file:
+                                try:
+                                    data = json.load(file)
+                                except json.JSONDecodeError:
+                                    data = {}
+
+                        else:
+                            data = {}
+
+                        entry = {
+                            "passID": passId,
+                            "password": generate_pwd,
+                            "date": date
+                        }
+
+                        data[label] = entry
+
+                        with open(filename, "w") as file:
+                            json.dump(data, file, indent=4)                        
+
+                        state4 = False 
+
+                    elif optionConFile == 'n' or optionConFile == 'no':
+                        print("Its okay we can create a new one from scratch.")
+                        # Funcion para crear un archivo
+
+                        empty_data = {}
+
+                        with open("passwords.json", "w") as file:
+                            json.dump(empty_data, file, indent=4)
+
+                        print("Empty passwords.json file created.")
+                        
+                        activation = True
+                        state4 = False
+
+                        # Debo conectar esto con lo otro de arriba para continuar con el proceso correctamente...
+
+                    else:
+                        print("Sorry but your response doesnt match with none of our options, try again.")
+
+
+                passId = input(f"The following password: {generate_pwd}, corresponds to which Site or App? (This is to identify): ")
+                date = datetime.now().isoformat()
+                
+                # Aqui se debe recopilar passID, Passwd, Date, Versions
+
+
+
+                state3 = False
+                
+            elif optionConfirm == "n" or optionConfirm == "no":
+                print("Thanks for your response. ")
+                state3 = False
+            else:
+                print("Somethings wrong with your answer... please type y/n to confirm.")
+    
 if __name__ == "__main__":
     password_gen = GenPassword()
 
@@ -77,6 +161,8 @@ if __name__ == "__main__":
 
     generate_pwd = password_gen.generatePass()
     print(f"Generated Password: {generate_pwd}")
+
+    password_gen.getStorage()
 
 
 
